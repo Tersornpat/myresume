cf0f12a910fa7db52f802329e3e56519596bba7d
Tersornpat <sornpat@gmail.com>
2024-06-27 19:24:52 +0700

    add icon

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <main className="content">
           <section id="home" className="background-lg" style={{ paddingTop: `${navHeight + height}px` }}>
             <Home />
-            <img className='img-spliter' src='https://media.discordapp.net/attachments/1115203267954221116/1254822331830763652/Untitled.jpg?ex=667e2f9d&is=667cde1d&hm=0d2f82f9398ec8603ec8940412a9f055f4e9fceb2ce415598f3137111f77e2c9&=&format=webp&width=1152&height=123'></img>
+            <img className='img-spliter' src='https://media.discordapp.net/attachments/1115203267954221116/1254822331830763652/Untitled.jpg?ex=667e2f9d&is=667cde1d&hm=0d2f82f9398ec8603ec8940412a9f055f4e9fceb2ce415598f3137111f77e2c9&=&format=webp&width=1152&height=123' alt='wave'></img>
           </section>
           <section id="about" style={{ paddingTop: `${navHeight + height}px` }}>
             <About />
