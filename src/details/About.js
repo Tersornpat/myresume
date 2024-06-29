53997fed8abc994af2896a9db184e8f5e8452c08
Tersornpat <sornpat@gmail.com>
2024-06-29 22:11:46 +0700

    add animate

--- src/details/About.js
+++ src/details/About.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import { React, useState , useEffect } from 'react';
 import './css/style.css'
 
-import youngman from '../img/about_img/youngman.jpg'
+import youngman from '../img/about_img/tersornpat2.jpg'
 
 function About() {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const handleLoad = () => setIsLoaded(true);
+        window.addEventListener('load', handleLoad);
+        return () => window.removeEventListener('load', handleLoad);
+    }, []);
     return (
-        <div className='container px-3 px-lg-4'>
-            <div className='row'>
-                <div className='col-lg-6 mt-5'>
-                    <p>Hello, my name is Sornpat Amthiangtham.</p>
-                    <p>I am a new graduate and aspiring full-stack developer, holding a Bachelor's degree in Computer Science from King Mongkut's Institute of Technology Ladkrabang (KMITL).</p>
-                    <p>I am enthusiastic about applying my knowledge and skills to your projects, and I'm eager to contribute to creating efficient solutions that address business needs.</p>
-                </div>
-                <div className='col-lg-6 d-flex justify-content-center align-items-center mt-3'>
-                    <img className='profile-resize rounded' src={youngman} alt='Profile' />
+        <div className='pt-4 px-3 px-lg-4'>
+            <div className='container-narrow'>
+                <div className='row'>
+                    <div className='col-lg-6 mt-5 md-6' >
+                        <h2>Hello, I'm Sornpat Amthiangtham.</h2>
+                        <p>I am a new graduate and aspiring full-stack developer, holding a Bachelor's degree in Computer Science from King Mongkut's Institute of Technology Ladkrabang (KMITL).</p>
+                        <p>I am enthusiastic about applying my knowledge and skills to your projects, and I'm eager to contribute to creating efficient solutions that address business needs.</p>
+                    </div>
+                    <div className={`col-lg-6 d-flex justify-content-center align-items-center mt-3 fade-in-sectionX ${isLoaded ? 'loaded' : ''}`}>
+                        <img className='profile-resize rounded' src={youngman} alt='Profile' />
+                    </div>
                 </div>
             </div>
         </div>
--- src/details/Home.js
+++ src/details/Home.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import { React, useState , useEffect } from 'react';
 import { SocialIcon } from 'react-social-icons';
 import './css/style.css';
 
 import penguin from '../img/home_img/penguin.png'
 
 function Home() {
+
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const handleLoad = () => setIsLoaded(true);
+        window.addEventListener('load', handleLoad);
+        return () => window.removeEventListener('load', handleLoad);
+    }, []);
+
     return (
         <div className='container px-3'>
             <div className='row'>
                 <div className='col-lg-6 p-2 d-flex justify-content-center align-items-center'>
                     <img className='image-resize' src={penguin}></img>
                 </div>
-                <div className='col-lg-6'>
+                <div className={`col-lg-6 fade-in-sectionY ${isLoaded ? 'loaded' : ''}`}>
                     <div className='mt-5'>
                         <p className='lead text-uppercase mb-1 text-left'>
                             Hello!
@@ -23,17 +32,18 @@ function Home() {
                             Full Stack Web Developer
                         </p>
                         <div className='home-contact'>
-                            <SocialIcon bgColor="black" fgColor="white" url="https://github.com/tersornpat" />
-                            <SocialIcon bgColor="black" fgColor="white" href="#contact" network="email" />
-                            <SocialIcon bgColor="black" fgColor="white" url="https://www.linkedin.com/in/tersornpat" />
-                            <SocialIcon bgColor="black" fgColor="white" url="https://www.facebook.com/tersornpat" />
+                            <SocialIcon className='social-icon' bgColor="black" fgColor="white" url="https://github.com/tersornpat" />
+                            <SocialIcon className='social-icon' bgColor="black" fgColor="white" href="#contact" network="email" />
+                            <SocialIcon className='social-icon' bgColor="black" fgColor="white" url="https://www.linkedin.com/in/tersornpat" />
+                            <SocialIcon className='social-icon' bgColor="black" fgColor="white" url="https://www.facebook.com/tersornpat" />
                         </div>
                         <div className='mt-5'>
                             <a href='#contact'>
-                                <button type='button' className='btn btn-warning hover-effect'>Contact Me</button>
+                                <button type='button' className='btn btn-warning btn-lg hover-effect custom-button'>
+                                    Contact Me
+                                </button>
                             </a>
                         </div>
-
                     </div>
                 </div>
             </div>
